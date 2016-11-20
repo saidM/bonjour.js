28f00c29c4f8f31c0a54636739ff2c6550134cb8
Saïd <sayid.mimouni@gmail.com>
2016-11-20 02:57:39 +0100

    Add basic HTTP server

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Hapi = require('hapi'),
+      server = new Hapi.Server(),
+      Translator = require('./lib/translator')
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.route({
+  method: 'GET',
+  path: '/translations/{locale}/{key}',
+  handler: (request, reply) => {
+    Translator.get(request.params.locale, request.params.key)
+    .then(data => reply(data))
+    .catch(err => reply({ error: err }).code(404))
+  }
+})
+
+server.start((err) => {
+  if (err) throw err
+  console.log('Server running on port 8000...')
+})
+
